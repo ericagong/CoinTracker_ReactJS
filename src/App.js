80e4724c01a60466629ee2620bd2b20b7da0da97
ericagong <48196721+ericagong@users.noreply.github.com>
2022-03-19 13:01:01 +0900

    coin tracker with basic information

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json)
+        setLoading(false)
+      })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Coin Tracker</h1>
+      {!loading ? (
+        <h4>You can see {coins.length} coins information right here!</h4>
+      ) : null}
+      <hr />
+      {loading ? <strong>Loading...</strong> : null}
+      <ul>
+        {coins.map((coin, index) => (
+          <li key={index}>
+            {coin.rank}. {coin.name} ({coin.symbol}) {coin.quotes.USD.price} USD
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
